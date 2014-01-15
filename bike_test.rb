78aef5556e779db58eac860f3a13b7d3b5a00a5a
Mike Gehard <mike.gehard@gmail.com>
2014-01-14 17:21:36 -0700

    Are we done yet?

--- bike_test.rb
+++ bike_test.rb
@@ -18,6 +18,25 @@ def test_color
   end
 end
 
+def test_red_bikes_are_cool
+  #Setup
+  bike = Bike.new("red")
+
+  # What I want the code to do.
+  expected = true
+
+  # What the code actually does.
+  actual = bike.cool?
+
+  # Do they match up.
+  if expected == actual
+    puts "."
+  else
+    raise "Test Failed! <#{expected}> != <#{actual}>"
+  end
+end
+
 # Run the tests by calling the methods
 test_color
+test_red_bikes_are_cool
 
