1c60dad327324b2715dee140cb341f2ba7efae20
Mike Gehard <mike.gehard@gmail.com>
2014-01-18 10:40:52 -0700

    Requirements change.

--- bike_test.rb
+++ bike_test.rb
@@ -18,27 +18,9 @@ def test_color
   end
 end
 
-def test_red_bikes_are_cool
+def test_red_bikes_are_not_cool
   #Setup
   bike = Bike.new("red")
-
-  # What I want the code to do.
-  expected = true
-
-  # What the code actually does.
-  actual = bike.cool?
-
-  # Do they match up.
-  if expected == actual
-    puts "."
-  else
-    raise "Test Failed! <#{expected}> != <#{actual}>"
-  end
-end
-
-def test_blue_bikes_are_not_cool
-  #Setup
-  bike = Bike.new("blue")
 
   # What I want the code to do.
   expected = false
@@ -54,7 +36,25 @@ def test_blue_bikes_are_not_cool
   end
 end
 
+def test_blue_bikes_are_cool
+  #Setup
+  bike = Bike.new("blue")
+
+  # What I want the code to do.
+  expected = true
+
+  # What the code actually does.
+  actual = bike.cool?
+
+  # Do they match up.
+  if expected == actual
+    puts "."
+  else
+    raise "Test Failed! <#{expected}> != <#{actual}>"
+  end
+end
+
 # Run the tests by calling the methods
 test_color
-test_red_bikes_are_cool
-test_blue_bikes_are_not_cool
+test_red_bikes_are_not_cool
+test_blue_bikes_are_cool
