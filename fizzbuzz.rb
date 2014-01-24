aba2a19c019690695da1c9acb13e6350a387a04c
Mike Gehard <mike.gehard@gmail.com>
2014-01-24 09:10:13 -0700

    Return the correct numbers based on initialization.

---
+++ fizzbuzz.rb
@@ -0,0 +1,9 @@
+class FizzBuzz
+  def initialize(upper_bound)
+    @upper_bound = upper_bound
+  end
+
+  def run
+    1.upto(@upper_bound).to_a
+  end
+end
--- fizzbuzz_spec.rb
+++ fizzbuzz_spec.rb
@@ -1,2 +1,14 @@
 require './fizzbuzz'
 require 'rspec/core'
+
+describe FizzBuzz do
+  it 'returns a list of numbers based on initialization' do
+    fizzbuzz = FizzBuzz.new(5)
+
+    expected = [1, 2, 3, 4, 5]
+
+    actual = fizzbuzz.run
+
+    expect(actual).to eq expected
+  end
+end
