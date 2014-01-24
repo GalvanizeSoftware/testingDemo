8f4e86902277761cb5b607aa00e83b6aa1ddbf26
Mike Gehard <mike.gehard@gmail.com>
2014-01-24 10:21:17 -0700

    Remove duplication.

--- key_value_store_spec.rb
+++ key_value_store_spec.rb
@@ -2,38 +2,38 @@ require './key_value_store'
 require 'rspec/core'
 
 describe KeyValueStore do
-  it 'allows me to get a key and get it back' do
-    store = KeyValueStore.new
-    store.add('student', 'gSchool')
+  before do
+    @store = KeyValueStore.new
+  end
 
-    expect(store.get('student')).to eq 'gSchool'
+  it 'allows me to get a key and get it back' do
+    @store.add('student', 'gSchool')
+
+    expect(@store.get('student')).to eq 'gSchool'
   end
 
   it 'allows for deletion of a key' do
-    store = KeyValueStore.new
-    store.add('student', 'gSchool')
+    @store.add('student', 'gSchool')
 
-    store.delete('student')
-    expect(store.get('student')).to be_nil
+    @store.delete('student')
+    expect(@store.get('student')).to be_nil
   end
 
   it 'can be queried for the existing keys' do
-    store = KeyValueStore.new
-    store.add('student', 'gSchool')
-    store.add('teacher', 'Mike')
+    @store.add('student', 'gSchool')
+    @store.add('teacher', 'Mike')
 
-    expect(store.keys).to eq ['student', 'teacher']
+    expect(@store.keys).to eq ['student', 'teacher']
   end
 
   it 'can be cleared' do
-    store = KeyValueStore.new
-    store.add('student', 'gSchool')
-    store.add('teacher', 'Mike')
+    @store.add('student', 'gSchool')
+    @store.add('teacher', 'Mike')
 
-    store.clear
+    @store.clear
 
-    expect(store.keys).to eq []
-    expect(store.get('student')).to be_nil
-    expect(store.get('teacher')).to be_nil
+    expect(@store.keys).to eq []
+    expect(@store.get('student')).to be_nil
+    expect(@store.get('teacher')).to be_nil
   end
 end
