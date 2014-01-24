a5aae502c25fede1cbf08bf1e1a29cd0a525bc57
Mike Gehard <mike.gehard@gmail.com>
2014-01-24 09:36:49 -0700

    If statements can return value.

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -8,20 +8,23 @@ class FizzBuzz
     result = []
 
     numbers.each do |number|
-      if multiple_of?(3, number) && multiple_of?(5,number)
-        result << "FizzBuzz"
+      substitution = if multiple_of?(3, number) && multiple_of?(5,number)
+        "FizzBuzz"
       elsif multiple_of?(3, number)
-        result << "Fizz"
+        "Fizz"
       elsif multiple_of?(5,number)
-        result << "Buzz"
+        "Buzz"
       else
-        result << number
+        number
       end
+
+      result << substitution
     end
     result
   end
 
   private
+
   def multiple_of?(divided_by, number_in_question)
     number_in_question % divided_by == 0
   end
